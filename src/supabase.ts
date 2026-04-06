50691ed4fc0f0b9e652cbb1caa3ba89773664286
grin114-픽셀 <grin114@gmail.com>
2026-04-06 15:05:24 +0900

    fix: manual supabase update

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// 1. 여기에 지연님의 진짜 주소를 넣으세요 (따옴표 안에!)
+// ⚠️ 아래 '작은따옴표' 안에 지연님의 진짜 정보를 넣으세요!
 const supabaseUrl = 'https://ygcltfujdnqiewkvgpze.supabase.co'
-
-// 2. 여기에 지연님의 진짜 키를 넣으세요 (따옴표 안에!)
 const supabaseKey = 'sb_publishable_gNpNZlootvjtyk4xKqLX-w_R8FWjGQV'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
