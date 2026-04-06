5dc5be452730cd1b9b0766e8ce81abd7fcf101f8
grin114-픽셀 <grin114@gmail.com>
2026-04-06 14:58:05 +0900

    fix: define supabase variables

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-// 수파베이스 대시보드에서 복사한 값들을 여기에 넣으세요!
-const SUPABASE_URL = 'https://ygcltfujdnqiewkvgpze.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_gNpNZlootvjtyk4xKqLX-w_R8FWjGQV'
+// 1. 주소와 키를 변수에 담습니다 (따옴표 필수!)
+const supabaseUrl = 'https://github.com/grin114-pixel/trip_money.git'
+const supabaseKey = 'git@github.com:grin114-pixel/trip_money.git
+'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+// 2. 위에서 만든 변수를 사용해서 금고를 엽니다
+export const supabase = createClient(supabaseUrl, supabaseKey)
