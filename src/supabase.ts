b8b33e25c7a67c10ea334d799829dfa79a8dd434
grin114-픽셀 <grin114@gmail.com>
2026-04-06 15:02:01 +0900

    fix: final check

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-// 1. 주소와 키를 변수에 담습니다 (따옴표 필수!)
-const supabaseUrl = 'https://github.com/grin114-pixel/trip_money.git'
-const supabaseKey = 'git@github.com:grin114-pixel/trip_money.git
-'
+// 1. 여기에 지연님의 진짜 주소를 넣으세요 (따옴표 안에!)
+const supabaseUrl = 'https://ygcltfujdnqiewkvgpze.supabase.co'
 
-// 2. 위에서 만든 변수를 사용해서 금고를 엽니다
+// 2. 여기에 지연님의 진짜 키를 넣으세요 (따옴표 안에!)
+const supabaseKey = 'sb_publishable_gNpNZlootvjtyk4xKqLX-w_R8FWjGQV'
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
